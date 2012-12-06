ec390c7e21c69acf1a4884fcb5430f31773de5e7
Scott Lampert <sl724q@att.com>
2012-12-06 00:59:18 +0000

    * Uses hard coded versioning to grab a tagged version from Brightcove * Gutted some more code and removed the git install method. If we ever   need it we can add it back in. * Updated the README with default attribute info

--- attributes/install.rb
+++ attributes/install.rb
@@ -2,3 +2,4 @@ default[:diamond][:required_python_packages] = { "configobj" => "4.7.2","psutil"
 default[:diamond][:required_debian_packages] = ["pbuilder","python-mock","python-configobj","cdbs","python-pysnmp4"]
 default[:diamond][:git_tmp] = "/mnt/git"
 default[:diamond][:git_path] = "#{node[:diamond][:git_tmp]}/Diamond"
+default[:diamond][:git_repository_uri] = "https://github.com/BrightcoveOS/Diamond.git"
