1de5c42b2f1033211f17a909be0f01e6560755ca
Scott Lampert <sl724q@att.com>
2012-12-12 00:17:12 +0000

    Fixed install issues in brand new environment.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "sl724q@att.com"
 license          "Apache 2.0"
 description      "Installs/Configures diamond"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.0.10"
+version          "2.0.11"
 
 supports         'ubuntu'
 supports         'debian'
