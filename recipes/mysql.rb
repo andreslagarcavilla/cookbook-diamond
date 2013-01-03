5a760908da62c61650c357fedc6f486fa8fdb2a7
Scott Lampert <sl724q@att.com>
2013-01-03 11:18:16 -0800

    Convert symbols to strings.

--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -6,11 +6,11 @@ include_recipe 'diamond::default'
 #mysql = Chef::EncryptedDataBagItem.load("passwords", "mysql")
 
 collector_config "MySQLCollector" do
-  path    node[:diamond][:collectors][:MySQLCollector][:path]
-  host    node[:diamond][:collectors][:MySQLCollector][:host]
-  port    node[:diamond][:collectors][:MySQLCollector][:port]
-  db      node[:diamond][:collectors][:MySQLCollector][:db]
-  user    node[:diamond][:collectors][:MySQLCollector][:user]
-  passwd  node[:diamond][:collectors][:MySQLCollector][:password]
+  path    node['diamond']['collectors']['MySQLCollector']['path']
+  host    node['diamond']['collectors']['MySQLCollector']['host']
+  port    node['diamond']['collectors']['MySQLCollector']['port']
+  db      node['diamond']['collectors']['MySQLCollector']['db']
+  user    node['diamond']['collectors']['MySQLCollector']['user']
+  passwd  node['diamond']['collectors']['MySQLCollector']['password']
 end
 
