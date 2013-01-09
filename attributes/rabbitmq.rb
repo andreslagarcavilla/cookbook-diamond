e09a281f308d36e3651b7db7d493ce2644b80522
Scott Lampert <sl724q@att.com>
2013-01-08 19:28:53 -0800

    Added sensible defaults for rabbitmq

---
+++ attributes/rabbitmq.rb
@@ -0,0 +1,4 @@
+default['diamond']['collectors']['RabbitMQCollector']['user'] = 'guest'
+default['diamond']['collectors']['RabbitMQCollector']['password'] = 'guest'
+default['diamond']['collectors']['RabbitMQCollector']['host'] = 'localhost:5672'
+default['diamond']['collectors']['RabbitMQCollector']['path'] = 'rabbitmq'
