28f81e70d37bec1889ea352b16ec7573698221d3
Scott Lampert <scott@lampert.org>
2013-02-28 10:29:35 -0800

    Remove cruft that old versions of diamond needed.  New version is packaged better.

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,34 +11,18 @@ include_recipe "diamond::install"
 template "/etc/diamond/diamond.conf" do
   action :create
   source "diamond.conf.erb"
-  mode 0644
+  mode 00644
   owner "root"
   group "root"
   notifies :restart, "service[diamond]"
-end
-
-template "/etc/init/diamond.conf" do
-  source "diamond.erb"
-  mode 0755
-  owner "root"
-  group "root"
-  action :create_if_missing
 end
 
 template "/etc/default/diamond" do
   source "diamond.default.erb"
-  mode 0644
+  mode 00644
   owner "root"
   group "root"
   notifies :restart, "service[diamond]"
-end
-
-cookbook_file "/etc/init.d/diamond" do
-  source "diamond.init"
-  mode 0755
-  owner "root"
-  group "root"
-  action :create_if_missing
 end
 
 # Install collectors
@@ -49,5 +33,5 @@ end
 service "diamond" do
   provider Chef::Provider::Service::Upstart
   supports :status => true, :restart => true
-  action [:enable, :start]
+  action [:enable]
 end
